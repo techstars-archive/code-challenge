9d009dbbf03e4f0410b9faf4afd91ce4fb78374a
Jennifer Barstow <jen.e.humphrey@gmail.com>
2017-10-07 12:41:27 -0600

    added views and crud for companies

--- codeChallenge/app/controllers/companies_controller.rb
+++ codeChallenge/app/controllers/companies_controller.rb
@@ -17,8 +17,26 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to company_path(@company)
+    else
+      render :edit
+    end
+  end
+
   def show
     @company = Company.find(params[:id])
+  end
+
+  def destroy
+    Company.destroy(params[:id])
+    redirect_to companies_path
   end
 
   private
